34e61e539150e648bf737aeaa0a8f479d8cf7d08
nishu-murmu <nishumurmu017@gmail.com>
2026-07-27 20:00:40 +0530

    chore: Update .gitignore to include target directory and .pem files; adjust JSON and URL encoding limits in server index; change API redirect URL

--- apps/server/src/index.ts
+++ apps/server/src/index.ts
@@ -49,8 +49,8 @@ app.use(
   })
 );
 app.use(apiRateLimiter);
-app.use(express.json({ limit: "50mb" }));
-app.use(express.urlencoded({ limit: "50mb", extended: true }));
+app.use(express.json({ limit: "10mb" }));
+app.use(express.urlencoded({ limit: "10mb", extended: true }));
 app.use(cookieParser());
 
 app.get("/healthcheck", (_req: Request, res: Response) => {
